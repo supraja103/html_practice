5966556d81a0e6a2f5cc1db2dc83c915ee402a08
supraja103 <suprajadesula103@gmail.com>
2023-11-30 10:09:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def one(request):
+    return render(request,'one.html')
+def two(request):
+    return render(request,'two.html')
+def three(request):
+    return render(request,'three.html')
+def fourth(request):
+    return render(request,'fourth.html')
+def fifth(request):
+    return render(request,'fifth.html')
+def sixth(request):
+    return render(request,'sixth.html')
+def seventh(request):
+    return render(request,'seventh.html')
+def eighth(request):
+    return render(request,'eighth.html')
+def nineth(request):
+    return render(request,'nineth.html')
+def tenth(request):
+    return render(request,'tenth.html')
